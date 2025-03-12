b2a7c7607c01384addcd3fdba59beb22f85afbe4
Aonurie <146367830+Aonurie@users.noreply.github.com>
2025-03-11 21:36:24 -0400

    Update and rename dynamicInput.tsx to InputWrapper.tsx

--- components/InputWrapper.tsx
+++ components/InputWrapper.tsx
@@ -1,4 +1,9 @@
+"use client";
+
 import dynamic from "next/dynamic";
 
 const Input = dynamic(() => import("./input"), { ssr: false });
-export default Input;
+
+export default function InputWrapper() {
+  return <Input />;
+}
