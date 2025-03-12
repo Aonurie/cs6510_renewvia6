29ebc2a92c54f4251398c8d869c7ec76b2abeb2b
Aonurie <146367830+Aonurie@users.noreply.github.com>
2025-03-11 20:26:37 -0400

    Update input.tsx

--- components/input.tsx
+++ components/input.tsx
@@ -2,7 +2,7 @@
 
 import { useState } from "react";
 
-export default function ExcelProcessor() {
+export default function input() {
   const [excelFile, setExcelFile] = useState<File | null>(null);
   const [costPoles, setCostPoles] = useState("");
   const [costMVCables, setCostMVCables] = useState("");
