d6c8f807af91eb917e620013829e5c35ab3355ab
Aonurie <146367830+Aonurie@users.noreply.github.com>
2025-03-11 21:06:12 -0400

    Update about.tsx

--- content/pages/about.tsx
+++ content/pages/about.tsx
@@ -1,9 +1,13 @@
----
-title: Power Grid Generation Project
----
+import dynamic from "next/dynamic";
 
-import Input from "../../components/dynamicInput";
+const Input = dynamic(() => import("../components/input"), { ssr: false });
 
-Use the form below to upload an Excel file and provide cost inputs:
-
-<Input />
+const AboutPage = () => {
+  return (
+    <div>
+      <h1>Power Grid Generation Project</h1>
+      <p>Use the form below to upload an Excel file and provide cost inputs:</p>
+      <Input />
+    </div>
+  );
+};
