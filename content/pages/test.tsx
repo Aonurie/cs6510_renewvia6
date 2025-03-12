e253c72b4a2526fdcf4e629eea66469a44b6ce1b
Aonurie <146367830+Aonurie@users.noreply.github.com>
2025-03-12 01:55:08 -0400

    Create test.tsx

---
+++ content/pages/test.tsx
@@ -0,0 +1,10 @@
+import DynamicUploadWrapper from '@/components/DynamicUploadWrapper';
+
+export default function TestPage() {
+  return (
+    <div>
+      <h1>Test Upload Form</h1>
+      <DynamicUploadWrapper />
+    </div>
+  );
+}
