350483ce7b0e64f5d82cb9f6af9f946e39fbc53d
tboyd44 <taylormorganboyd@gmail.com>
2025-03-15 23:27:24 -0700

    adding project updates

--- app/page.tsx
+++ app/page.tsx
@@ -4,14 +4,27 @@ import Link from "next/link"
 export default function Home() {
   return (
     <div className="prose dark:prose-invert">
-      {allPosts.map((post) => (
-        <article key={post._id}>
-          <Link href={post.slug}>
-            <h2>{post.title}</h2>
-          </Link>
-          {post.description && <p>{post.description}</p>}
-        </article>
-      ))}
+      {/* Static Content */}
+      <section>
+        <h1>P6 Renewvia-Design</h1>
+        <p>
+        The Renewvia Energy project aims to develop an electricity grid design optimizer using machine learning. The goal is to optimize pole and cable placement and cost to provide reliable electricity for areas in Kenya.
+
+        Our deliverables include an approach to solving the graph problem and comprehensive project documentation in the form of a research paper.
+        </p>
+      </section>
+
+      {/* Dynamic List of Posts */}
+      <section>
+        {allPosts.map((post) => (
+          <article key={post._id}>
+            <Link href={post.slug}>
+              <h2>{post.title}</h2>
+            </Link>
+            {post.description && <p>{post.description}</p>}
+          </article>
+        ))}
+      </section>
     </div>
   )
 }
