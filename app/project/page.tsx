d53730128a7eeefce92af6a5d772a272cf918a9f
tboyd44 <taylormorganboyd@gmail.com>
2025-03-17 14:27:47 -0700

    Added updates and tried to fix image on Proj goals & updates page

--- app/project/page.tsx
+++ app/project/page.tsx
@@ -11,7 +11,21 @@ export async function generateMetadata() {
 export default function ProjectPage() {
   return (
     <div style={{ padding: "2rem" }}>
-      <h1>Upload Excel and Enter Costs</h1>
+      <p style={{ marginBottom: "1rem", fontSize: "1rem", color: "#333" }}>
+        This page allows users to enter their own test data and cost hyperparameters for an optimized energy grid design.<br /><br />
+        
+        <strong>Test data:</strong> An Excel file with GPS coordinates for buildings & a power source (solar grid) in a community. The file must have the following columns:
+        <ul>
+          <li><strong>name</strong> (indicating 'Building' or 'Power Source')</li>
+          <li><strong>latitude</strong></li>
+          <li><strong>longitude</strong></li>
+        </ul>
+
+        <strong>Cost hyperparameters:</strong> Pole cost, MV Cable cost, LV Cable cost (all in USD).<br /><br />
+
+        <strong>Output:</strong> A visual of an optimal grid design based on given data.
+      </p>
+      <h1 style={{ marginBottom: "1rem" }}>Upload Test GPS Coordinate Data and Enter Test Costs</h1>
       <Input />
     </div>
   );
